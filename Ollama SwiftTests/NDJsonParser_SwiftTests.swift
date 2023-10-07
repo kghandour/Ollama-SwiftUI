05c226a8ab1b7f7e64515e6478bd66c6de8ae7c0
Karim ElGhandour <kghandour@live.com>
2023-10-07 23:13:33 +0200

    Test for NDJSON parser
    Need to still work on the testperformance

---
+++ Ollama SwiftTests/NDJsonParser_SwiftTests.swift
@@ -0,0 +1,56 @@
+
+import Foundation
+import XCTest
+@testable import Ollama_Swift
+
+final class NDJsonParser_SwiftTests: XCTestCase {
+    
+    let ndJson: String = "{ \"key\": \"value\" }\n"
+    
+    func testSimpleNDJSON() {
+        let twoLines = ndJson + ndJson
+        XCTAssert(parseDataFromString(string: twoLines).count == 2)
+    }
+    
+    func testEmptyNDJSON() {
+        XCTAssert(parseDataFromString(string: "").count == 0)
+    }
+    
+    func testDoubleNewLineNDJSON() {
+        let doubleNewLines = ndJson + "\n" + ndJson
+        XCTAssert(parseDataFromString(string: doubleNewLines).count == 2)
+    }
+    
+    func testOneNewLineNDJSON() {
+        let doubleNewLines = "\n"
+        XCTAssert(parseDataFromString(string: doubleNewLines).count == 0)
+    }
+    
+    func testLeadingNewLineNDJSON() {
+        let leadingNewLine = "\n" + ndJson
+        XCTAssert(parseDataFromString(string: leadingNewLine).count == 1)
+    }
+    
+    func testNoEndingNewLine() {
+        let noEndingNewLine = "{ \"key\": \"value\" }"
+        XCTAssert(parseDataFromString(string: noEndingNewLine).count == 1)
+    }
+    
+//    func testPerformance() {
+//        let huge = Array(count: 100000, repeatedValue: ndJson).reduce("", combine: +)
+//        self.measureBlock {
+//            self.parseDataFromString(huge)
+//        }
+//    }
+    
+    private func parseDataFromString(string: String) -> [AnyObject] {
+        let data = string.data(using: String.Encoding.utf8)!
+        do {
+            let parsed = try JSONParser.JSONObjectsWithData(data: data, options: [])
+            return try JSONSerialization.jsonObject(with: parsed, options: []) as! [AnyObject]
+        } catch {
+            XCTFail("Error")
+            return []
+        }
+    }
+}
