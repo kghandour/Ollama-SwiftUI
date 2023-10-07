6234f2fb39e162f531eb72ec9983978083a38f64
Karim ElGhandour <kghandour@live.com>
2023-10-07 13:55:23 +0200

    Initial Commit

---
+++ Ollama Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ollama Swift
+//
+//  Created by Karim ElGhandour on 07.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ollama Swift/Ollama_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ollama_SwiftApp.swift
+//  Ollama Swift
+//
+//  Created by Karim ElGhandour on 07.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Ollama_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
