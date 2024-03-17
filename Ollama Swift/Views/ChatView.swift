3b60baf601085d65c37709ef04b1b3e890ba202d
Karim ElGhandour <kghandour@live.com>
2024-03-17 14:07:49 +0200

    Fix scroll to bottom automatically

--- Ollama Swift/Views/ChatView.swift
+++ Ollama Swift/Views/ChatView.swift
@@ -12,108 +12,124 @@ struct ChatView: View {
     @StateObject var chatController = ChatController()
     
     @FocusState private var promptFieldIsFocused: Bool
+    @Namespace var bottomId
     
     var body: some View {
         VStack(spacing: 0)
         {
-            ScrollView {
-                Text("This is the start of your chat")
-                    .foregroundStyle(.secondary)
-                    .padding()
-                ForEach(Array(chatController.sentPrompt.enumerated()), id: \.offset) { idx, sent in
-                    ChatBubble(direction: .right) {
-                        Markdown {
-                            .init(sent.trimmingCharacters(in: .whitespacesAndNewlines))
+            ScrollViewReader{ sv in
+                ScrollView {
+                    Text("This is the start of your chat")
+                        .foregroundStyle(.secondary)
+                        .padding()
+                    ForEach(Array(chatController.sentPrompt.enumerated()), id: \.offset) { idx, sent in
+                        ChatBubble(direction: .right) {
+                            Markdown {
+                                .init(sent.trimmingCharacters(in: .whitespacesAndNewlines))
+                            }
+                            .markdownTextStyle{
+                                ForegroundColor(Color.white)
+                            }
+                            .padding([.leading, .trailing])
+                            .padding([.top, .bottom], 8)
+                            .textSelection(.enabled)
+                            .background(Color.blue)
                         }
-                        .markdownTextStyle{
-                            ForegroundColor(Color.white)
+                        ChatBubble(direction: .left) {
+                            Markdown {
+                                .init(chatController.receivedResponse.indices.contains(idx) ?
+                                      chatController.receivedResponse[idx].trimmingCharacters(in: .whitespacesAndNewlines) :
+                                        "...")
+                            }
+                            .markdownTextStyle(\.code) {
+                                FontFamilyVariant(.monospaced)
+                                BackgroundColor(.white.opacity(0.25))
+                            }
+                            .markdownBlockStyle(\.codeBlock) { configuration in
+                                configuration.label
+                                    .padding()
+                                    .markdownTextStyle {
+                                        FontFamilyVariant(.monospaced)
+                                    }
+                                    .background(Color.white.opacity(0.25))
+                            }
+                            .padding([.leading, .trailing])
+                            .padding([.top, .bottom], 8)
+                            .textSelection(.enabled)
+                            .foregroundStyle(Color.secondary)
+                            .background(Color(NSColor.secondarySystemFill))
                         }
-                        .padding([.leading, .trailing])
-                        .padding([.top, .bottom], 8)
-                        .textSelection(.enabled)
-                        .background(Color.blue)
                     }
-                    ChatBubble(direction: .left) {
-                        Markdown {
-                            .init(chatController.receivedResponse.indices.contains(idx) ?
-                                  chatController.receivedResponse[idx].trimmingCharacters(in: .whitespacesAndNewlines) :
-                                    "...")
-                        }
-                        .markdownTextStyle(\.code) {
-                            FontFamilyVariant(.monospaced)
-                            BackgroundColor(.white.opacity(0.25))
-                        }
-                        .markdownBlockStyle(\.codeBlock) { configuration in
-                            configuration.label
-                                .padding()
-                                .markdownTextStyle {
-                                    FontFamilyVariant(.monospaced)
-                                }
-                                .background(Color.white.opacity(0.25))
-                        }
-                        .padding([.leading, .trailing])
-                        .padding([.top, .bottom], 8)
-                        .textSelection(.enabled)
-                        .foregroundStyle(Color.secondary)
-                        .background(Color(NSColor.secondarySystemFill))
-                    }
+                    Text("")
+                        .id(bottomId)
+                }
+                .onChange(of: chatController.receivedResponse.last) { _, _ in
+                    sv.scrollTo(bottomId)
+                    
                 }
             }
-            .defaultScrollAnchor(.bottom)
-            HStack {
-                TextField("Enter system prompt...", text: $chatController.prompt.system, axis: .vertical)
-                    .lineLimit(3)
-                    .disabled(chatController.disabledEditor)
-                    .textFieldStyle(.roundedBorder)
-            }
-            .padding()
-            .background(.ultraThickMaterial)
-            .frame(height: chatController.expandOptions ? nil : 0)
-            .clipped()
-            HStack(){
-                ZStack(alignment: .topLeading) {
-                    if chatController.prompt.prompt.isEmpty {
-                        Text("Enter prompt...")
-                            .foregroundColor(Color.gray)
-                            .padding(.leading, 10)
-                    }
-                    TextEditor(text: $chatController.prompt.prompt)
-                        .padding(.leading, 5)
-                        .frame(minHeight: 35, maxHeight: 200)
-                        .foregroundColor(.primary)
-                        .dynamicTypeSize(.medium ... .xxLarge)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .opacity(chatController.prompt.prompt.isEmpty ? 0.75 : 1)
-                        .onChange(of: chatController.prompt.prompt) { newValue, _ in
-                            chatController.disabledButton = newValue.isEmpty
-                        }
-                        .focused(self.$promptFieldIsFocused)
+            VStack{
+                VStack{
+                    TextField("Enter system prompt...", text: $chatController.prompt.system, axis: .vertical)
+                        .lineLimit(3)
                         .disabled(chatController.disabledEditor)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
                 }
-                .frame(minHeight: 36)
-                
-                Button {
-                    chatController.send()
-                } label: {
-                    Image(systemName: "paperplane")
-                        .frame(width: 20, height: 20, alignment: .center)
-                        .foregroundStyle(.blue)
-                }
-                .disabled(chatController.disabledButton)
-                
-                Button {
-                    chatController.resetChat()
-                } label: {
-                    Image(systemName: "trash")
-                        .frame(width: 20, height: 20, alignment: .center)
-                }
+                .frame(height: chatController.expandOptions ? nil : 0)
+                .clipped()
                 Button{
                     withAnimation {
                         chatController.expandOptions.toggle()
                     }
                 } label: {
-                    Image(systemName: "rectangle.expand.vertical")
-                        .frame(width: 20, height: 20, alignment: .center)
+                    if(!chatController.expandOptions){
+                        Text("More Options")
+                        Image(systemName: "arrow.up")
+                            .frame(width: 20, height: 20, alignment: .center)
+                    }else{
+                        Text("Hide Options")
+                        Image(systemName: "arrow.down")
+                            .frame(width: 20, height: 20, alignment: .center)
+                    }
+                }
+                HStack(){
+                    ZStack(alignment: .topLeading) {
+                        if chatController.prompt.prompt.isEmpty {
+                            Text("Enter prompt...")
+                                .foregroundColor(Color.gray)
+                                .padding(.leading, 10)
+                        }
+                        TextEditor(text: $chatController.prompt.prompt)
+                            .padding(.leading, 5)
+                            .frame(minHeight: 35, maxHeight: 200)
+                            .foregroundColor(.primary)
+                            .dynamicTypeSize(.medium ... .xxLarge)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .opacity(chatController.prompt.prompt.isEmpty ? 0.75 : 1)
+                            .onChange(of: chatController.prompt.prompt) { newValue, _ in
+                                chatController.disabledButton = newValue.isEmpty
+                            }
+                            .focused(self.$promptFieldIsFocused)
+                            .disabled(chatController.disabledEditor)
+                    }
+                    .frame(minHeight: 36)
+                    
+                    Button {
+                        chatController.send()
+                    } label: {
+                        Image(systemName: "paperplane")
+                            .frame(width: 20, height: 20, alignment: .center)
+                            .foregroundStyle(.blue)
+                    }
+                    .disabled(chatController.disabledButton)
+                    
+                    Button {
+                        chatController.resetChat()
+                    } label: {
+                        Image(systemName: "trash")
+                            .frame(width: 20, height: 20, alignment: .center)
+                    }
                 }
             }
             .padding(6)
